d09513fc3b2c5d98db4fde07b58075d3adef0b55
inizio <digit4free@gmail.com>
2016-10-18 21:53:38 +0800

    search bar shortcut

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div id='app'>
     <header class="header">
-      <input tag="h1" to="/" class="title search-bar" v-model="keyword" @keyup="goToSearch" onclick="this.select()">
+      <input tag="h1" to="/" class="title search-bar" v-model="keyword" @keyup.esc="resetSearch" onclick="this.select()">
     </header>
     <transition appear name="fade" out-in><router-view></router-view></transition>
   </div>
 </template>
 
 <style>
-/*@import url('https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:100|Lato:100');*/
 @import url('https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:100|Lato:200');
 @import url('https://fonts.googleapis.com/css?family=Raleway:400');
 </style>
@@ -28,7 +27,13 @@
       this.keyword = !this.$route.query.keyword || this.$route.query.keyword === this.title ? this.title : this.$route.query.keyword
     },
     methods: {
-      goToSearch: function () {
+      resetSearch: function () {
+        this.keyword = this.title
+        document.getElementsByClassName('search-bar')[0].blur()
+      }
+    },
+    watch: {
+      'keyword': function () {
         this.$router.push({name: 'list', query: {'keyword': this.keyword === this.title ? '' : this.keyword}})
       }
     }
