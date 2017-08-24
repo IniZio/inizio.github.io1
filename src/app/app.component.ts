ec63e5148f877eb77c3f9bf224de4164d1f21172
inizio <newman.kcchow@gmail.com>
2017-08-24 23:54:29 +0800

    Angular-cli initialize

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: String = 'Tour of heros';
+  heros: String[] = ['Winston', 'Tracer', 'Soldier'];
+}
